4ad2a617b7bdd27ce6f8ebfa0a0444cd59b3c7d7
Joumana El Alaoui <joumana.codes@gmail.com>
2024-11-21 14:13:22 -0600

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
 
 // Ensures gofmt doesn't remove the "net" import in stage 1 (feel free to remove this!)
 var _ = net.ListenUDP
+
+type DNSHeader struct {
+	ID      uint16
+	Flags   uint16
+	QDCOUNT uint16
+	ANCOUNT uint16
+	NSCOUNT uint16
+	ARCOUNT uint16
+}
+
+const DNSHeaderSize = 12
+
+func (h DNSHeader) ToBytes() []byte {
+	headerSlice := make([]byte, DNSHeaderSize)
+
+	// BigEndian saves the most significant piece of data at the lowest in memory
+	// Endian is useful when data isn't single-byte. In our case, each element is multiple bytes
+	binary.BigEndian.PutUint16(headerSlice[0:2], 1234)
+	binary.BigEndian.PutUint16(headerSlice[2:4], h.Flags)
+	binary.BigEndian.PutUint16(headerSlice[4:6], h.QDCOUNT)
+	binary.BigEndian.PutUint16(headerSlice[6:8], h.ANCOUNT)
+	binary.BigEndian.PutUint16(headerSlice[8:10], h.NSCOUNT)
+	binary.BigEndian.PutUint16(headerSlice[10:12], h.ARCOUNT)
+	return headerSlice
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -40,9 +66,22 @@ func main() {
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
 
 		// Create an empty response
-		response := []byte{}
+
+		var h DNSHeader
+		h.ID = 1234
+		h.Flags = 1 << 7
+		h.QDCOUNT = 0
+		h.ANCOUNT = 0
+		h.NSCOUNT = 0
+		h.ARCOUNT = 0
+
+		response := h.ToBytes()
+
+		// binary.BigEndian.PutUint16(response[0:2], 1234)
+		response[2] = 1 << 7
 
 		_, err = udpConn.WriteToUDP(response, source)
+
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
